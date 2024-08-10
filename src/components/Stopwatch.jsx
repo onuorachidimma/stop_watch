1fb38145c39fcd1dd3f7866f4b777e2e53a41533
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-08-10 15:04:30 +0100

    responsive styling to stopwatch app

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -49,29 +49,32 @@ const Stopwatch = () => {
   const formattedTime = timeFormat(time);
 
   return (
-    <div className="relative z-20 text-black align-center text-center bg-white w-[90%] md:w-[70%] lg:w-[50%] mx-auto rounded-bl-2xl rounded-br-2xl p-8">
-      <div className="grid grid-cols-4 gap-4 text-4xl font-bold">
-        <div>
-          <div>{formattedTime.hours}</div>
-          <div className="text-sm">Hour</div>
+    <div className="relative z-20 text-black align-center text-center bg-white w-[90%] md:w-[80%] lg:w-[60%] mx-auto rounded-bl-2xl rounded-br-2xl p-8">
+      <div className="flex justify-between border-b pb-4 mb-4">
+        <div className=''>
+          <div className='text-2xl sm:text-6xl md:text-8xl font-bold'>{formattedTime.hours}</div>
+          <div className="text-xxs sm:text-sm uppercase">Hour</div>
         </div>
+        <div className='text-2xl sm:text-6xl md:text-8xl font-bold'>:</div>
         <div>
-          <div>{formattedTime.minutes}</div>
-          <div className="text-sm">Minute</div>
+          <div className='text-2xl sm:text-6xl md:text-8xl font-bold'>{formattedTime.minutes}</div>
+          <div className="text-xxs sm:text-sm uppercase">Minute</div>
         </div>
+        <div className='text-2xl sm:text-6xl md:text-8xl font-bold'>:</div>
         <div>
-          <div>{formattedTime.seconds}</div>
-          <div className="text-sm">Second</div>
+          <div className='text-2xl sm:text-6xl md:text-8xl font-bold'>{formattedTime.seconds}</div>
+          <div className="text-xxs sm:text-sm uppercase">Second</div>
         </div>
+        <div className='text-2xl sm:text-6xl md:text-8xl font-bold'>:</div>
         <div>
-          <div>{formattedTime.milliseconds}</div>
-          <div className="text-sm">Millisecond</div>
+          <div className='text-2xl sm:text-6xl md:text-8xl font-bold'>{formattedTime.milliseconds}</div>
+          <div className="text-xxs sm:text-sm uppercase">Millisecond</div>
         </div>
       </div>
-      <div className="mt-8 space-x-4">
-        <button onClick={start} className="">Start</button>
-        <button onClick={stop} className="">Stop</button>
-        <button onClick={reset} className="">Reset</button>
+      <div className="flex justify-center gap-[20px] ">
+        <button onClick={start} className="bg-gray-800 text-xxs sm:text-base text-white py-2 px-6 sm:px-12 font-bold">Start</button>
+        <button onClick={stop} className="bg-gray-800 text-xxs sm:text-base text-white py-2 px-6 sm:px-12 font-bold">Stop</button>
+        <button onClick={reset} className="bg-gray-800 text-xxs sm:text-base text-white py-2 px-6 sm:px-12 font-bold">Reset</button>
       </div>
     </div>
   );
